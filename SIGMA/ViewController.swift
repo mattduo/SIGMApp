b948a04a08f7db7974b2618ffc613ae710b3f817
Matthew Duong <mduo0003@student.monash.edu>
2022-04-28 01:49:04 +1000

    Initial Commit

---
+++ SIGMA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SIGMA
+//
+//  Created by Matt on 28/4/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
